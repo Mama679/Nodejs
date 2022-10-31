bc49744ef4ea2cf25907317f88fd1b4e7cb4ffb1
Mario Manotas <mrmanotas@personalsoft.com.co>
2022-10-31 16:06:48 -0500

    Pagina web statica

--- webserver/app.js
+++ webserver/app.js
@@ -1,8 +1,25 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-http.createServer((req,res)=>{
-    res.write('<h1>Hola Mundo<h1>');
-    res.end();
-}).listen(4005);
+//Servir contenido estatico
 
-console.log("Escuchando por el puerto: 4005");
+app.use(express.static('public'));
+
+app.get('/elements', (req, res) =>{
+  res.sendFile(__dirname + '/public/elements.html');
+});
+
+app.get('/generic', (req, res) =>{
+    res.sendFile(__dirname + '/public/generic.html');
+  });
+
+app.get('*', (req, res) =>{
+   // res.send('404 | Page not found');
+   res.sendFile(__dirname + '/public/404.html');
+  });
+  
+  //app.listen(3000);
+  app.listen(port, () =>{
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
